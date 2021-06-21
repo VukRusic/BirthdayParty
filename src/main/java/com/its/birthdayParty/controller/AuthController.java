7326624f03046efc55a06e3505682040c88dcf13
Vuk <vukrusic1@gmail.com>
2021-06-22 01:33:40 +0200

    Login validation added

--- src/main/java/com/its/birthdayParty/controller/AuthController.java
+++ src/main/java/com/its/birthdayParty/controller/AuthController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.its.birthdayParty.model.Korisnik;
 import com.its.birthdayParty.service.AgencijaService;
@@ -38,19 +40,35 @@ public class AuthController {
 		return "redirect:/";
 	}
 	
-	@PostMapping("/LogIn")
-	public String LogIn(@RequestParam String email, @RequestParam String sifra,HttpServletRequest request) {
+	/*
+	 * @PostMapping("/LogIn") public String LogIn(@RequestParam String
+	 * email, @RequestParam String sifra, HttpServletRequest request) { Korisnik
+	 * korisnik = korisnikService.login(email, sifra);
+	 * 
+	 * if(korisnik != null) { request.getSession().setAttribute("user", korisnik);
+	 * if(korisnik.getTip() == "klijent") { return "redirect:/"; } else { return
+	 * "redirect:/"; } } else { return "redirect:/test"; } }
+	 */
+	
+	@GetMapping("/LogIn")
+	public String login() {
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/checkLogin", method = RequestMethod.POST, produces="text/plain")
+	@ResponseBody
+	public String foo(@RequestParam String email, @RequestParam String sifra,HttpServletRequest request) {
 		Korisnik korisnik = korisnikService.login(email, sifra);
 		
 		if(korisnik != null) {
 		request.getSession().setAttribute("user", korisnik);
 			if(korisnik.getTip() == "klijent") {
-				return "redirect:/";
+				return "true";
 			} else {
-				return "redirect:/";
+				return "true";
 			}
 		} else {
-			return "redirect:/";
+			return "false";
 		}
 	}
 	
